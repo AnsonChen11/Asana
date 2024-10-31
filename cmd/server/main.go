6c52173eccf0f1dc27502d072bac466c28cca382
anson.chen <ab67325@gmail.com>
2024-10-31 20:46:21 +0800

    chore: CORS setting

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,16 +5,27 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Go server with Gorilla Mux is running in docker through CICD on 8080 10/31!")
+	fmt.Fprintf(w, "Hello, Go server with Gorilla Mux is running!")
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", helloHandler).Methods("GET")
+
+	// Allow requests from the front-end domain
+	allowedOrigins := handlers.AllowedOrigins([]string{
+		"http://74.179.58.253:8080",
+		"http://74.179.58.253:8082",
+	})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"})
+	allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
+
+	// Start the server
 	fmt.Println("Server started at :8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r)))
 }
