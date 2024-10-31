e7b4fdb700960a94130c9010930fc3f2e238121b
anson.chen <ab67325@gmail.com>
2024-10-31 15:41:30 +0800

    test: deploy to dev VM

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,12 +9,12 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Go server with Gorilla Mux is running in docker through CICD in dev environment!")
+	fmt.Fprintf(w, "Hello, Go server with Gorilla Mux is running in docker through CICD on 8080 10/31!")
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", helloHandler).Methods("GET")
-	fmt.Println("Server started at :8082")
-	log.Fatal(http.ListenAndServe(":8082", r))
+	fmt.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
